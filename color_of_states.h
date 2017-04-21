f5f604e396b4837f6cca3572fa90e2b64ef8c9ab
Esteban Jenkins <stbnjenkins@gmail.com>
2017-04-20 21:00:39 -0600

    colores provincias

---
+++ color_of_states.h
@@ -0,0 +1,38 @@
+// MORADO
+const double SAN_JOSE_R = 0.6;
+const double SAN_JOSE_G = 0.4;
+const double SAN_JOSE_B = 1;
+
+// ROJO
+const double ALAJUELA_R = 1;
+const double ALAJUELA_G = 0;
+const double ALAJUELA_B = 0;
+
+// AZUL
+const double CARTAGO_R = 0;
+const double CARTAGO_G = 0;
+const double CARTAGO_B = 1;
+
+
+// AMARILLO
+const double HEREDIA_R = 1;
+const double HEREDIA_G = 1;
+const double HEREDIA_B = 0;
+
+
+// VERDE
+const double LIMON_R = 0;
+const double LIMON_G = 1;
+const double LIMON_B = 0;
+
+
+// CAFE
+const double GUANACASTE_R = 0.43;
+const double GUANACASTE_G = 0.27;
+const double GUANACASTE_B = 0.002;
+
+// ANARANJADO
+const double PUNTARENAS_R = 1;
+const double PUNTARENAS_G = 0.4;
+const double PUNTARENAS_B = 0;
+
